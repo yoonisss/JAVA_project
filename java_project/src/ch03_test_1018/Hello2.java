b58b1014ae17ab7561c52fd7ab5a426d260065fb
yoonisss <kyong4962@naver.com>
2022-11-24 13:56:11 +0900

    hello java

---
+++ java_project/src/ch03_test_1018/Hello2.java
@@ -0,0 +1,21 @@
+package ch03_test_1018;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 실행은 ctrl + f11
+		// 주석은 보동 ctrl +/ : 단일 행 주석 
+		// 한번 더 ctrl +/ : 취소가 됨.
+		
+		//여러 행 주석 : ctrl + shift + /
+		//여러 행 주석 해제 : ctrl  + shift + \
+		//자동 포맷 정렬 : ctrl + shift + f
+		
+		
+		  System.out.println("Hello 이상용 자바");
+		  System.out.println("Hello 이상용 자바");
+		 
+	}
+
+}
